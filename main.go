2fbf3e9f5672ec58ad8195b70f3eff5c993d5cd7
Johnman67112 <joaovitorcucomo@gmail.com>
2023-03-28 21:17:37 -0300

    base hello handler

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/Johnman67112/fx-api/ui"
+	"go.uber.org/fx"
+)
+
+func main() {
+	fx.New(
+		fx.Provide(ui.NewHTTPServer, ui.NewServeMux, ui.NewEchoHandler),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
+}
